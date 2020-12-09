2b9ced8a14d0f0f4f596993f9fdbfced457f70ab
dodinovembri <dodinovembri32@gmail.com>
2020-12-09 08:56:45 +0700

    update logo

--- resources/views/templates/backend/sidebar.blade.php
+++ resources/views/templates/backend/sidebar.blade.php
@@ -4,12 +4,9 @@
             <div class="brand">
                 <a href="{{ url('home') }}" class="logo">
                     <span>
-                        <img src="{{ asset('backend/default/assets/images/logo-sm.png') }}" alt="logo-small" class="logo-sm">
+                        <img src="{{ asset('img/logo2.jpeg') }}" alt="logo-small" class="logo-sm">
                     </span>
-                    <span>
-                        <img src="{{ asset('backend/default/assets/images/logo.png') }}" alt="logo-large" class="logo-lg logo-light">
-                        <img src="{{ asset('backend/default/assets/images/logo-dark.png') }}" alt="logo-large" class="logo-lg logo-dark">
-                    </span>
+
                 </a>
             </div>
             <!--end logo-->
